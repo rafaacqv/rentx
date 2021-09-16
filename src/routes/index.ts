5d354e9c0fdcf2dfedb3d1eaf15ac46ff31ba72c
Rafael Acquaviva <rafaacqv@gmail.com>
2021-09-16 00:22:43 -0300

    Create user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificatiosRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificatiosRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
