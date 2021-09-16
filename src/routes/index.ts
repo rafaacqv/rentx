c4d1ccda9c879e4b900cb6e7e7caaf0623d002fb
Rafael Acquaviva <rafaacqv@gmail.com>
2021-09-16 03:05:17 -0300

    Generate JSON web token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificatiosRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificatiosRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
