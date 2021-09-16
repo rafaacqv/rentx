92b24f0f65b8d83068bb1194c6fcbed2a9d3c27c
Rafael Acquaviva <rafaacqv@gmail.com>
2021-09-16 04:30:14 -0300

    Error handling implemented

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -14,10 +16,21 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error : ${err.message}`,
+    });
+  }
+);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running"));
